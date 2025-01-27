6449da0073e40ebe5013472520ef7e8abb2d9e08
Mark Ortleb <markortleb@gmail.com>
2025-01-26 20:08:07 -0500

    adaadad

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
+        allowedHosts: [
+            'pizza-cafe.markortleb.com',  // Allow requests from this hostname
+            'localhost',                  // Allow localhost
+            '127.0.0.1'                   // Allow requests from localhost IP
+        ],
     },
     module: {
         rules: [
